65850d0a55a228a768992f3018d16f473182f285
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-08 16:14:31 -0400

    Add files via upload

---
+++ src/services/transactionServices.js
@@ -0,0 +1,16 @@
+
+const date = new Date().toISOString();
+
+export class TransactionService {
+    constructor(){
+        this.transactions = new Map();
+        this.initializeSampleData();
+    }
+
+    initializeSampleData(){
+        const sampleDate = [{
+            id: 1,
+
+        }]
+    }
+}
