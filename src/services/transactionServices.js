4eed0d61ebc2823a4c0bfbbd9d300a8f80b07c85
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-08 16:19:13 -0400

    Update transactionServices.js

--- src/services/transactionServices.js
+++ src/services/transactionServices.js
@@ -1,5 +1,5 @@
 
-const date = new Date().toISOString();
+const formatDate = new Date().toISOString();
 
 export class TransactionService {
     constructor(){
@@ -10,7 +10,31 @@ export class TransactionService {
     initializeSampleData(){
         const sampleDate = [{
             id: 1,
-
+            date: formatDate,
+            type: "credit",
+            category: "job",
+            amount: 100000,  // In banking systems the amount is treated in "int" and not in "float" because of precision issues
+            description: "salary", 
+            createdAt: formatDate,
+            updatedAt: formatDate
+        }, {           
+            id: 2,
+            date: formatDate,
+            type: "debit",
+            category: "food",
+            amount: 3000,  
+            description: "food", 
+            createdAt: formatDate,
+            updatedAt: formatDate
+        }, {
+            id: 3,
+            date: formatDate,
+            type: "credit",
+            category: "side gig",
+            amount: 50000,  
+            description: "side gig", 
+            createdAt: formatDate,
+            updatedAt: formatDate
         }]
     }
 }
