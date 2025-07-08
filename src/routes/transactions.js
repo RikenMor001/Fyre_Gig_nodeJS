ababdb3b23a8abd43488c7899d4f8a4d946a666a
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-08 14:51:03 -0400

    Add files via upload

---
+++ src/routes/transactions.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const router = express.Router();
+
+// GET/transactions
+router.get("/transactions", async (req, res, next) => {
+
+})
+
+// GET/transactions/:id
+router.get("transactions/:id", async(req, res, next) => {
+
+})
+
+// POST /createTransaction
+router.post("createTransactions", async (req, res, next) => {
+
+})
+
+// PUT /updateTransaction
+router.put("updateTransaction", async (req, res, next) => {
+
+})
+
+// DELETE /deleteTransaction
+router.delete("deleteTransaction", async (req, res, next) => {
+
+})
+
+module.exports = router;
