da033d9d0c5f8a26e2f341c2dd91f01d1979d662
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-08 23:05:11 -0400

    Update validation.js

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,10 +1,18 @@
 const { z } = require("zod");
 
-const transactionScheme = z.object({
+const transactionSchema = z.object({
     amount: z.number()
     .positive("Account balance cannot be negative")
     .max(1000000 + "Account balance should not exceed 10,00,000"),
-    type: z.enum(["credit" | "debit"], {
+    
+    type: z.enum(["credit", "debit"], {
         errorMap: () => ({message: "Type must be either credit or debit"})
-    })
+    }),
+
+    descritpion: z.string()
+    .min(1, "Description cannot be empty")
+    .max(255, "No more than 255 words should be used")
+    .trim(),
+      
 })
+
