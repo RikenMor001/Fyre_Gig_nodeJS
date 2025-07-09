73534e672e6d8a4e8ac9bc748cca23477ebd0a13
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-08 23:20:15 -0400

    Update validation.js

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -13,6 +13,33 @@ const transactionSchema = z.object({
     .min(1, "Description cannot be empty")
     .max(255, "No more than 255 words should be used")
     .trim(),
-      
+
+    category: z.string()
+    .min(1, "Minimum 1 category required")
+    .max(50, "No more than 50 characters")
+    .trim(),
+
+    account: z.string()
+    .min(1, "Account cannot be empty")
+    .max(50, "No more than 50 charatcers")
+    .trim(),
+
+    reference: z.string()
+    .max(100, "Reference cannot exceed 100 characters")
+    .trim()
+    .optional(),
+    tags: z.array(z.string().trim().max(20)).max(5).optional()
 })
+
+const transactionUpdateSchema = transactionSchema.partial();
+
+export const validateTransaction = (data) => {
+    return transactionSchema.safeParse(data);
+}
+
+export const validateUpdateTransaction = (data) => {
+    return transactionUpdateSchema.safeParse(data)
+}
+
+export {transactionSchema, transactionUpdateSchema};
 
