175a1f2492687df95f311da4614fbb8fad7af5a8
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-10 21:33:07 -0400

    Update transactions.js

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,1 +1,15 @@
 
+const express = require("express");
+const { getAllTransactions } = require("../models/transaction");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    try {
+        const transaction = getAllTransactions();
+        res.json(transaction);  
+    } catch(error){
+        res.status(404).json({
+            message: "Not able to fetch all transactions"
+        })
+    }
+})
