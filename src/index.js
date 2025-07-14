24845733804ff47a290a4ee696a0dc33a8dc2fae
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-07-13 23:49:42 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 
+const express = require("express");
+const transactionRoutes = require("./routes/transactions");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use("/api/transactions", transactionRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
